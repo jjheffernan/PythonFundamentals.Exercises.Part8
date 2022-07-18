1156e74f8d3d15f22f1ed4ac924c7f17dc1f4859
jjheffernan <jjheffernan99@gmail.com>
2022-07-18 01:35:46 -0400

    added get_filename, and file output. by default it returns local directory

--- tree.py
+++ tree.py
@@ -1,16 +1,30 @@
 import sys
 import os
 
-filename = '.'
+filename = './'
+
+
+def get_filename(*args):
+    if len(sys.argv) > 2:
+        print('incompatible use of function')
+    global filename
+    filename = sys.argv[1]
+    return filename  # escape return
 
 
 def get_files(filename):
     # files = os.listdir(filename)
     for root, dirs, files in os.walk(filename, False, ):
         for name in files:
-            print(os.path.join(root, name))
+            # print(os.path.join(root, name))
+            print_file(root, name)
+            f_out.writelines(fr"{root + name}" + " \n")
         for name in dirs:
-            print(os.path.join(root, name))
+            # print(os.path.join(root, name))
+            print_dict(root, name)
+            f_out.writelines(f"{root + name} \n")
+    # close file to clean up
+    f_out.close()
 
     # for item in files:
     #     spec_file = print_dict() + item
@@ -24,26 +38,32 @@ def get_files(filename):
     # pass
 
 
-def print_dict():
-    return os.getcwd()
+def print_dict(root, name):
+    # return os.getcwd()
+    print(os.path.join(root, name))
     # pass
 
 
-def print_file():
-    files = [get_files()]
-    print(files)
-    # for expansion
+def print_file(root, name):
+    # files = [get_files()]
+    # print(files)
+    print(os.path.join(root, name))
+    # return os.path.join(root, name)
+    # for expansion, doesnt work
 
 
 def main(filename):
     # find_dir = input('input directory to print: ')
-    print(str(get_files(filename)))
+    # print(str(get_files(filename)))
     # print_dict()
+    get_files(filename)
     return
 
 
 if __name__ == '__main__':
-    filename = sys.argv[0]
-    if filename is None:
-        filename = '/test'
+    # filename = sys.argv[0]
+    # if filename is None:
+    #     filename = '.'
+    f_out = open('directory_log.txt', 'w')
+    f_out.write('Start of directory list: \n')
     main(filename)
